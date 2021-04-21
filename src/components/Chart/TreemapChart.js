a959fe1810b57705268d9a8ccc031d7d3cce651e
Saroopashree Kumaraguru <saroopa25@gmail.com>
2021-04-21 15:33:06 +0530

    Remove mock data from TreemapChart

--- src/components/Chart/TreemapChart.js
+++ src/components/Chart/TreemapChart.js
@@ -1,27 +1,6 @@
 import React, { Component } from "react";
 import ApexChart from "react-apexcharts";
 
-const data = [
-  {
-    id: "men",
-    label: "men",
-    value: 32.53792484936965,
-    color: "#468df3",
-  },
-  {
-    id: "women",
-    label: "women",
-    value: 3.079252346235851,
-    color: "#ba72ff",
-  },
-  {
-    id: "children",
-    label: "children",
-    value: 14.163650187828225,
-    color: "#a1cfff",
-  },
-];
-
 class TreemapChart extends Component {
   constructor(props) {
     super(props);
